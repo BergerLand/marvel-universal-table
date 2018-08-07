9b90039b6ec9de3113f1102c4433b2c264b13d7e
Phillip R. Berger <phillip@berger.land>
2018-08-07 11:02:46 -0400

    Updated controller to returning the flat table.

--- bin/controller.js
+++ bin/controller.js
@@ -2,22 +2,6 @@ var controller = (() => {
 
     const table = require("./table.json");
 
-    const getTable = () => {
-        return table;
-    };
-
-    const getFeats = () => {
-        return table.feats;
-    };
-
-    const getRanks = () => {
-        return table.ranks;
-    };
-
-    return {
-        getModel: getModel
-    }
-
 })();
 
-module.exports = controller;
+module.exports = controller.table;
